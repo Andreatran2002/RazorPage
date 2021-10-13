68fd2da77fe2524667d6767b403e5339cb98bfe0
andreatran2002 <andreatran2002@gmail.com>
2021-10-13 08:47:07 +0700

    l

--- Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
+    
 namespace entity_fr.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
